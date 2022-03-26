dc7d02a77a9c3871926a10d2564fcd121ff7a5e1
Wesley Soares <wesleybs90@gmail.com>
2022-03-26 02:29:38 -0300

    Adicionar protótipo tela de Login

--- frontend/src/app/views/login/login.component.ts
+++ frontend/src/app/views/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    public formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.construirForm();
   }
 
+  construirForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      senha: ['', [Validators.required]],
+    });
+  }
+
+  teste(event){
+    console.log(event)
+    console.log(this.loginForm.valid)
+    console.log(this.loginForm.get('email')?.valid)
+  }
 }
