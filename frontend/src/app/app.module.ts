91462a8532628fe236a6d51adbda0d88690689c5
Wesley Soares <wesleybs90@gmail.com>
2022-03-26 01:44:57 -0300

    Adicionar protótipo tela de Cadastro de Produtor e ajustes no projeto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,11 @@ import { HomeComponent } from './views/home/home.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { CadastroComponent } from './views/cadastro/cadastro.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './views/login/login.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +19,17 @@ import { FooterComponent } from './components/template/footer/footer.component';
     HomeComponent,
     NavComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CadastroComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
