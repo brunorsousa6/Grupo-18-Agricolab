142916cde796ff02828d1aa2709818404e6d51db
Bruno Rios Sousa <brunoxz.10x@hotmail.com>
2022-03-26 15:16:15 -0300

    cognito back

--- agricolab/agricolab-aplication/src/main/java/br/com/agricolab/AgricolabApplication.java
+++ agricolab/agricolab-aplication/src/main/java/br/com/agricolab/AgricolabApplication.java
@@ -1,14 +1,48 @@
 package br.com.agricolab;
 
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.jwk.source.RemoteJWKSet;
+import com.nimbusds.jose.proc.JWSKeySelector;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
+import com.nimbusds.jose.util.DefaultResourceRetriever;
+import com.nimbusds.jose.util.ResourceRetriever;
+import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 @SpringBootApplication(scanBasePackages = {"br.com.agricolab", "br.com.agricolab.produtor.controller.Controller"})
 public class AgricolabApplication {
 
+	@Value("${com.agricolab.jwt.aws.connectionTimeout}")
+	public int connectionTimeout;
+
+	@Value("${com.agricolab.jwt.aws.readTimeout}")
+	public int readTimeout;
+
+	@Value("${com.agricolab.jwt.aws.jwkUrl}")
+	private String jwkUrl;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AgricolabApplication.class, args);
+	}
+
+	@Bean
+	public ConfigurableJWTProcessor configurableJWTProcessor() throws MalformedURLException {
+		ResourceRetriever resourceRetriever = new DefaultResourceRetriever(connectionTimeout, readTimeout);
+		URL jwkURL = new URL(jwkUrl);
+		JWKSource jwkSource = new RemoteJWKSet(jwkURL, resourceRetriever);
+		ConfigurableJWTProcessor jwtProcessor = new DefaultJWTProcessor();
+		JWSKeySelector keySelector = new JWSVerificationKeySelector(JWSAlgorithm.RS256, jwkSource);
+		jwtProcessor.setJWSKeySelector(keySelector);
+		return jwtProcessor;
 	}
 
 }
--- agricolab/agricolab-aplication/src/main/java/br/com/agricolab/controller/AgricolabController.java
+++ agricolab/agricolab-aplication/src/main/java/br/com/agricolab/controller/AgricolabController.java
@@ -1,6 +1,8 @@
 package br.com.agricolab.controller;
 
+import br.com.agricolab.dto.MessageDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("agricolab")
 public class AgricolabController {
 
-    @GetMapping
-    public ResponseEntity<?> hello(){
-        return ResponseEntity.ok("Hello World");
+    @GetMapping("/hello-admin")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<MessageDto> helloAdmin() {
+        return ResponseEntity.ok(new MessageDto("hola ADMIN desde cognito"));
     }
-    
+
+    @GetMapping("/hello-user")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public ResponseEntity<MessageDto> helloUser() {
+        return ResponseEntity.ok(new MessageDto("hola USER desde cognito"));
+    }
 }
