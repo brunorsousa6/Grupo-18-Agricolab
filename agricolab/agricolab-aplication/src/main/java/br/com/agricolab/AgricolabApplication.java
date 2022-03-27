4c68efc7b509f5ea6140c301dd68a93ae82766f7
Bruno Rios Sousa <brunoxz.10x@hotmail.com>
2022-03-27 01:02:02 -0300

    packages

--- agricolab/agricolab-aplication/src/main/java/br/com/agricolab/AgricolabApplication.java
+++ agricolab/agricolab-aplication/src/main/java/br/com/agricolab/AgricolabApplication.java
@@ -2,8 +2,14 @@ package br.com.agricolab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"br.com.agricolab", "br.com.agricolab.produtor.controller.ProdutorController", "br.com.agricolab.consumidor.controller.ConsumidorController"})
+@SpringBootApplication(scanBasePackages = {"br.com.agricolab.*", "br.com.agricolab.produtor.controller.ProdutorController", "br.com.agricolab.consumidor.controller.ConsumidorController"})
+@ComponentScan(basePackages = {"br.com.agricolab.*"})
+@EntityScan(basePackages = {"br.com.agricolab.*"})
+@EnableJpaRepositories(basePackages = {"br.com.agricolab.*"})
 public class AgricolabApplication {
 
 	public static void main(String[] args) {
